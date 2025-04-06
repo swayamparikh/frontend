73f02fd0b195dcd607a72285bd8ed801e9f5e2d4
swayamparikh <swayamparikh1@gmail.com>
2025-04-06 08:47:50 +0530

    helo

---
+++ Javascript/ex.js
@@ -0,0 +1,11 @@
+console.log("hello this external js");
+document.write("hello dom method");
+
+
+// bom :- browser object model
+// logic function 
+
+// alert("pls filed the form")
+// confirm("are you sure for this filed?")
+// prompt(" 5 + 5 => ?")
+// print()
